c29044468f72add67c040c2a28e8538ea88acf3d
heegyoon <hgkim@cubox.ai>
2024-04-08 12:40:29 +0900

    update

--- ias.sh
+++ ias.sh
@@ -11,11 +11,11 @@
 #SBATCH --cpus-per-task=32
 
 #SBATCH --container-image /purestorage/project/hgkim/enroot/images/ias.sqsh
-#SBATCH --container-mounts=/purestorage:/purestorage
+#SBATCH --container-mounts=/purestorage:/purestorage,/purestorage/project/hgkim/projects/IAS-KR:/workspace
 #SBATCH --container-writable
 
 torchrun \
     --nnodes=1 \
     --nproc_per_node=2 \
-    /purestorage/project/hgkim/projects/IAS-KR/main.py \
+    main.py \
     --cfg experiments/train_on_image_1.yaml
