2c9897532b502ad1242569bb35a15390f2fa5027
heegyoon <hgkim@cubox.ai>
2024-04-18 13:53:10 +0900

    update

--- data/ias_dataset.py
+++ data/ias_dataset.py
@@ -1,14 +1,14 @@
 import os
 import cv2
+import numpy as np
 import json
 from glob import glob
 
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
-from torchvision.transforms.functional import crop
-
-from torchvision.io import read_image
+# from torchvision.transforms.functional import crop
+# from torchvision.io import read_image
 
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -35,140 +35,116 @@ class IASDataset(Dataset):
         ])
         self.resize = transforms.Compose([
             transforms.Resize(
-                (cfg['resize']['height'], cfg['resize']['width']),
+                (cfg['size']['height'], cfg['size']['width']),
                 transforms.InterpolationMode.BICUBIC)
         ])
         
-        # Image path
+        # Prepare data
         img_paths = os.path.join(cfg['data_path'], dataset_name, '*.*')
         img_paths = glob(img_paths)
         self.img_paths = [i for i in img_paths if is_image_file(i)]
-        # Label
-        self.labels = []
-        self.id_bboxes = []
-        json_paths = [os.path.splitext(i)[0]+'.json' for i in self.img_paths]
-        for json_path in json_paths:
-            with open(json_path, 'r', encoding='cp949') as f:
-                annots = json.load(f)
-            if annots['spoof_type'] in LABEL_DICT['Real']:
-                self.labels.append(1)
-            elif annots['spoof_type'] in LABEL_DICT['Paper']:
-                self.labels.append(0)
-            elif annots['spoof_type'] in LABEL_DICT['Display']:
-                self.labels.append(0)
-            else:
-                raise ValueError(f'Invalid spoof type. {json_path}')
-            
-            if 'idcard_bbox' in annots:
-                bbox = annots['idcard_bbox']    # [top, bottom, left, right]
-                # bbox = [bbox[0], bbox[2], bbox[1]-bbox[0], bbox[3]-bbox[2]]
-            else:
-                bbox = None
-            self.id_bboxes.append(bbox)
+        self.json_paths = [os.path.splitext(i)[0]+'.json' for i in self.img_paths]
+        assert len(self.img_paths) == len(self.json_paths)
     
     def __len__(self):
         return len(self.img_paths)
     
     def imshow(self, img, fname=None):
-        # import matplotlib.pyplot as plt
-        # from torchvision.transforms.functional import to_pil_image
-        # _resize = transforms.Compose([
-        #     transforms.Resize(
-        #         (448,448),
-        #         transforms.InterpolationMode.BICUBIC)
-        # ])
-        # if resize:
-        #     img = _resize(img)
-        #     img2 = _resize(img2)
-        # p = to_pil_image(img)
-        # p2 = to_pil_image(img2)
-        # plt.subplot(211)
-        # plt.title(fname)
-        # plt.imshow(p, cmap='gray')
-        # plt.subplot(212)
-        # plt.imshow(p2, cmap='gray')
-        # plt.show()
-        
-        import numpy as np
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
-        h, s, v = cv2.split(hsv)
-        hsv_out = np.vstack([h, s, v])
-        cv2.imshow(f'{fname}', cv2.resize(img, dsize=None, fx=0.5, fy=0.5))
-        cv2.imshow('hsv', cv2.resize(hsv_out, dsize=None, fx=0.25, fy=0.25))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        
-        # img = resize(img)
-        # n = np.transpose(img.numpy(), [2,1,0])
-        # n = n[:,:,::-1]
-        # cv2.imshow('n', n)
+        if isinstance(img, np.ndarray):
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            cv2.imshow('img',img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        elif isinstance(img, torch.Tensor):
+            import matplotlib.pyplot as plt
+            from torchvision.transforms.functional import to_pil_image
+            plt.imshow(to_pil_image(img))
+            plt.show()
+        # import numpy as np
+        # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
+        # h, s, v = cv2.split(hsv)
+        # hsv_out = np.vstack([h, s, v])
+        # cv2.imshow(f'{fname}', cv2.resize(img, dsize=None, fx=0.5, fy=0.5))
+        # cv2.imshow('hsv', cv2.resize(hsv_out, dsize=None, fx=0.25, fy=0.25))
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
-    
-    def np_to_tensor(self, img):
-        img = torch.from_numpy(img)
-        img = torch.permute(img, [2,0,1])   # HWC -> CHW
-        return img
-    
-    def read_image(self, path):
-        # return: RGB, CxHxW Tensor
-        img = cv2.imread(path)  # torchvision이나 pil로 읽으면 돌아감
+
+    def read_data(self, idx):
+        # Image
+        img = cv2.imread(self.img_paths[idx])  # torchvision이나 pil로 읽으면 돌아감
         img = img[:,:,::-1].copy()  # BGR -> RGB
         if self.cfg['color'] == 'hsv':
             img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV_FULL)
+        # Label
+        with open(self.json_paths[idx], 'r', encoding='cp949') as f:
+            annots = json.load(f)
+        return img, annots
+    
+    def align_idcard(self, img, keypoints, cls, dsize_factor = None):
+        keypoints = np.array(keypoints)
+        if cls[0] == 0:
+            idcard_ratio = np.array((86, 54))
+        elif cls[0] == 1:
+            idcard_ratio = np.array((125, 88))
+        else:
+            raise ValueError(f'Wrong cls: {cls}')
+
+        if dsize_factor is None:
+            dsize_factor = round(np.sqrt(cv2.contourArea(np.expand_dims(keypoints, 1))) / idcard_ratio[0])
+
+        dsize = idcard_ratio * dsize_factor  # idcard size unit: mm
+        dst = np.array(((0, 0), (0, dsize[1]), dsize, (dsize[0], 0)), np.float32)
+
+        M = cv2.getPerspectiveTransform(keypoints.astype(np.float32), dst)
+        img = cv2.warpPerspective(img, M, dsize)
         return img
-    
-    def crop_patch(self, img):
-        N = self.cfg['n_patches']
-        H = self.cfg['patch_size']
-        W = self.cfg['patch_size']
-        patches = torch.zeros([N, 3, H, W])
-        for n in range(N):
-            patch = self.random_crop(img)
-            std = torch.std(patch.float(), dim=(1,2), keepdim=True)
-        return patches        
-    
-    def __getitem__(self, idx: int):
-        label = self.labels[idx]
-        img = self.read_image(self.img_paths[idx])
+
+    def preprocess_input(self, img, annots):
+        # Label real/fake
+        if annots['spoof_type'] in LABEL_DICT['Real']:
+            label = 1
+        elif annots['spoof_type'] in LABEL_DICT['Paper']:
+            label = 0
+        elif annots['spoof_type'] in LABEL_DICT['Display']:
+            label = 0
+        else:
+            raise ValueError(f'Invalid spoof type.')
         
-        # Crop idcard
-        bbox = self.id_bboxes[idx]  # [top, left, height, width]
-        if bbox is not None:
-            img = img[bbox[0]:bbox[1], bbox[2]:bbox[3], :]
+        # Aligend or cropped image
+        if self.cfg['type'] == 'aligned':
+            if 'idcard_kpts' in annots:
+                img = self.align_idcard(img, annots['idcard_kpts'], annots['cls'])
+        else:   # Crop from raw image
+            if 'idcard_bbox' in annots:
+                bbox = annots['idcard_bbox']    # [top, bottom, left, right]
+                img = img[bbox[0]:bbox[1], bbox[2]:bbox[3], :]
+        img = torch.permute(torch.from_numpy(img), [2,0,1])   # HWC -> CHW
         
-        img = self.np_to_tensor(img)
-        patches = torch.stack(
-            [self.random_crop(img) for _ in range(self.cfg['n_patches'])]
-        )
-        
-        # self.imshow(img, patches[0], resize=True,
-        #             fname=os.path.basename(self.img_paths[idx]))
-        
-        # Resize image
-        img = self.resize(img)
-        
-        # Normalize
-        mean = torch.mean(img.float(), dim=(1,2), keepdim=True)
-        std = torch.std(img.float(), dim=(1,2), keepdim=True)
-        img = (img - mean) / std
-        mean = torch.mean(patches.float(), dim=(2,3), keepdim=True)
-        std = torch.std(patches.float(), dim=(2,3), keepdim=True) + 1e-12
-        patches = (patches - mean) / std
-        
+        # Return dict
         data_dict = {'label': torch.tensor(label).float()}
         if self.cfg['input'] == 'image':
-            data_dict['input'] = img
+            img = self.resize(img)
+            mean = torch.mean(img.float(), dim=(1,2), keepdim=True)
+            std = torch.std(img.float(), dim=(1,2), keepdim=True)
+            img = (img - mean) / std
+            data_dict['input'] = img.detach()
         elif self.cfg['input'] == 'patch':
-            data_dict['input'] = patches
+            patches = torch.stack(
+                [self.random_crop(img) for _ in range(self.cfg['n_patches'])]
+            )
+            self.imshow(patches[0])
+            mean = torch.mean(patches.float(), dim=(2,3), keepdim=True)
+            std = torch.std(patches.float(), dim=(2,3), keepdim=True) + 1e-12
+            patches = (patches - mean) / std
+            data_dict['input'] = patches.detach()
         else:
             raise ValueError
-
-        # if torch.isnan(patches).sum().item() > 0:
-        #     print(f'dataset {self.img_paths[idx]}')
-        
         return data_dict
+    
+    def __getitem__(self, idx):
+        img, annots = self.read_data(idx)
+        return self.preprocess_input(img, annots)
 
 
 if __name__ == '__main__':
@@ -177,11 +153,12 @@ if __name__ == '__main__':
             'data_path': 'C:/Users/heegyoon/Desktop/data/IAS/kr/processed',
             'patch_size': 256,
             'n_patches': 9,
-            'resize': {'height': 224, 'width': 224},
-            'input': 'image',
-            'color': 'rgb'
+            'size': {'height': 128, 'width': 128},
+            'input': 'patch',
+            'color': 'rgb',
+            'type': 'raw'
         },
-        'IAS_cubox_train_230117_extra'
+        'cubox_4k_2211'
     )
     from torch.utils.data import DataLoader
     from tqdm import tqdm
--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,10 @@
 from .ias_model import IASModel
 from .fc_model import FCModel
+from .lbp_model import LBPModel
 
 
 MODEL_DICT = {
     'ias_model': IASModel,
     'fc_model': FCModel,
+    'lbp_model': LBPModel,
 }
